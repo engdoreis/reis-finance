5e8b76b86da2986e00d487862f4448cb613dc486
Douglas Reis <engdoreis@gmail.com>
2024-01-06 18:48:52 +0000

    Add unittests to the yahoo scraper
    Signed-off-by: Douglas Reis <engdoreis@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 mod brokers;
 mod scraper;
 
+#[cfg(test)]
+mod testutils;
+
 use anyhow::Result;
 use brokers::{Broker, Trading212};
 use scraper::{self as sc, Scraper, Yahoo};
@@ -11,8 +14,8 @@ fn main() -> Result<()> {
         "{:?}",
         broker.load_from_csv("resources/tests/input/trading212/2022.csv")?
     );
-    let mut yh = Yahoo::new("AAPL".to_string());
-    let data = yh.load(sc::Time::Month(6), None)?;
+    let mut yh = Yahoo::new("BRK".to_string());
+    let data = yh.load(sc::SearchBy::PeriodFromNow(sc::Time::Month(24)))?;
     println!("Quotes: {:?}", data.quotes()?);
     println!("Splits: {:?}", data.splits()?);
     println!("Dividends: {:?}", data.dividends()?);
