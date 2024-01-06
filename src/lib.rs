e23937e0044ccb9e5beac2c24390061a0064a7e4
Douglas Reis <engdoreis@gmail.com>
2024-01-06 20:23:13 +0000

    Split the project into a lib and a binary
    Signed-off-by: Douglas Reis <engdoreis@gmail.com>

---
+++ src/lib.rs
@@ -0,0 +1,5 @@
+pub mod brokers;
+pub mod scraper;
+
+#[cfg(test)]
+mod testutils;
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,8 @@
-mod brokers;
-mod scraper;
-
-#[cfg(test)]
-mod testutils;
+extern crate reis_finance_lib;
 
 use anyhow::Result;
-use brokers::{Broker, Trading212};
-use scraper::{self as sc, Scraper, Yahoo};
+use reis_finance_lib::brokers::{Broker, Trading212};
+use reis_finance_lib::scraper::{self as sc, Scraper, Yahoo};
 
 fn main() -> Result<()> {
     let broker = Trading212::new();
@@ -14,8 +10,11 @@ fn main() -> Result<()> {
         "{:?}",
         broker.load_from_csv("resources/tests/input/trading212/2022.csv")?
     );
-    let mut yh = Yahoo::new("BRK".to_string());
-    let data = yh.load(sc::SearchBy::PeriodFromNow(sc::Time::Month(24)))?;
+    let mut yh = Yahoo::new();
+    let data = yh.load(
+        "GOOGL".to_string(),
+        sc::SearchBy::PeriodFromNow(sc::Time::Month(24)),
+    )?;
     println!("Quotes: {:?}", data.quotes()?);
     println!("Splits: {:?}", data.splits()?);
     println!("Dividends: {:?}", data.dividends()?);
