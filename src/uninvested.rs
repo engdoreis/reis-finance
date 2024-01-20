e46ccd3ba370109e2dce7e1f04416b944846eb68
Douglas Reis <engdoreis@gmail.com>
2024-01-20 10:15:36 +0000

    Add test for uninvested cash
    Signed-off-by: Douglas Reis <engdoreis@gmail.com>

--- src/uninvested.rs
+++ src/uninvested.rs
@@ -30,7 +30,53 @@ impl Cash {
         Ok(self
             .data
             .select([col(schema::Columns::Amount.into()).sum()])
-            .with_column(lit("Cash").alias(schema::Columns::Ticker.into()))
+            .with_column(
+                lit::<&str>(schema::Type::Cash.into()).alias(schema::Columns::Ticker.into()),
+            )
             .collect()?)
+    }
+}
+
+#[cfg(test)]
+mod unittest {
+    use super::*;
+    use crate::schema::Action::*;
+    use crate::schema::Columns::*;
+    use polars::prelude::*;
+
+    #[test]
+    fn uninvested_cash_success() {
+        let actions: &[&str] = &[
+            Deposit.into(),
+            Buy.into(),
+            Buy.into(),
+            Sell.into(),
+            Dividend.into(),
+            Withdraw.into(),
+        ];
+        let orders = df! (
+            Action.into() => actions,
+            Ticker.into() => &["CASH", "GOOGL", "GOOGL", "GOOGL", "GOOGL", "CASH"],
+            Amount.into() => &[10335.1, 4397.45, 2094.56, 3564.86, 76.87, 150.00],
+        )
+        .unwrap();
+
+        let cash_type: &str = schema::Type::Cash.into();
+
+        let cash = Cash::new(orders)
+            .collect()
+            .unwrap()
+            .lazy()
+            .select([col(Ticker.into()), dtype_col(&DataType::Float64).round(4)])
+            .collect()
+            .unwrap();
+        assert_eq!(
+            df! (
+                Ticker.into() => &[cash_type],
+                Amount.into() => &[7334.82],
+            )
+            .unwrap(),
+            cash
+        );
     }
 }
