19dc760e28282912640338cf63373c9201708b5b
Douglas Reis <engdoreis@gmail.com>
2024-01-25 21:46:00 +0000

    Update the perpetual inventory test
    Signed-off-by: Douglas Reis <engdoreis@gmail.com>

--- src/perpetutal_inventory.rs
+++ src/perpetutal_inventory.rs
@@ -115,6 +115,7 @@ mod unittest {
     fn average_cost_success() {
         let actions: &[&str] = &[
             Buy.into(),
+            Dividend.into(),
             Buy.into(),
             Buy.into(),
             Sell.into(),
@@ -124,17 +125,17 @@ mod unittest {
             Sell.into(),
             Buy.into(),
         ];
-        let country: &[&str] = &[Usa.into(); 9];
-        let mut tickers = vec!["GOOGL"; 5];
+        let country: &[&str] = &[Usa.into(); 10];
+        let mut tickers = vec!["GOOGL"; 6];
         tickers.extend(vec!["APPL", "GOOGL", "APPL", "APPL"]);
         let ticker_str: &str = Ticker.into();
 
         let orders = df! (
             Action.into() => actions,
-            Qty.into() => [8.0, 4.0, 10.0, 4.0, 8.0, 5.70, 10.0, 3.0, 10.5],
+            Qty.into() => [8.0, 1.0, 4.0, 10.0, 4.0, 8.0, 5.70, 10.0, 3.0, 10.5],
             Ticker.into() => tickers,
             Country.into() => country,
-            Price.into() => &[34.45, 32.5, 36.0, 35.4, 36.4, 107.48, 34.3, 134.6, 95.60],
+            Price.into() => &[34.45, 1.34, 32.5, 36.0, 35.4, 36.4, 107.48, 34.3, 134.6, 95.60],
         )
         .unwrap();
 
