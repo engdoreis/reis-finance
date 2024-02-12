e010162f17794891f6326d9bd149ed5b269ff409
Douglas Reis <engdoreis@gmail.com>
2024-02-12 21:24:52 +0000

    Add command line arguments to the app
    Signed-off-by: Douglas Reis <engdoreis@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -13,21 +13,54 @@ use reis_finance_lib::summary::Summary;
 use reis_finance_lib::uninvested;
 use reis_finance_lib::{liquidated, IntoLazyFrame};
 
+use clap::Parser;
+use std::path::PathBuf;
+use std::str::FromStr;
+
+// Define a struct to represent command-line options
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// A folder with Trading 212 orders
+    #[arg(short, long, value_parser =  PathBuf::from_str)]
+    trading212_orders: Option<PathBuf>,
+
+    /// A folder with Schwab orders
+    #[arg(short, long, value_parser =  PathBuf::from_str)]
+    schwab_orders: Option<PathBuf>,
+
+    /// The currency to be used
+    #[arg(short, long, value_parser =  schema::Currency::from_str, default_value = "USD")]
+    currency: schema::Currency,
+}
+
 fn main() -> Result<()> {
     std::env::set_var("POLARS_FMT_TABLE_ROUNDED_CORNERS", "1"); // apply rounded corners to UTF8-styled tables.
     std::env::set_var("POLARS_FMT_MAX_COLS", "20"); // maximum number of columns shown when formatting DataFrames.
     std::env::set_var("POLARS_FMT_MAX_ROWS", "30"); // maximum number of rows shown when formatting DataFrames.
     std::env::set_var("POLARS_FMT_STR_LEN", "50"); // maximum number of characters printed per string value.
 
-    let broker = Schwab::default();
-    let schwab = broker.load_from_dir(std::path::Path::new("/tmp/schwab"))?;
+    let args: Args = Args::parse();
+    let mut orders: Vec<DataFrame> = Vec::new();
 
-    let broker = Trading212::default();
-    let t212 = broker.load_from_dir(std::path::Path::new("/tmp/trading212"))?;
-    execute(vec![t212, schwab])
+    if let Some(schwab_orders) = args.schwab_orders {
+        let broker = Schwab::default();
+        orders.push(broker.load_from_dir(schwab_orders.as_path())?);
+    }
+
+    if let Some(trading212_orders) = args.trading212_orders {
+        let broker = Trading212::default();
+        orders.push(broker.load_from_dir(trading212_orders.as_path())?);
+    }
+
+    if orders.len() > 0 {
+        execute(orders, args.currency)
+    } else {
+        anyhow::bail!("You must provide orders.")
+    }
 }
 
-fn execute(orders: Vec<impl IntoLazyFrame>) -> Result<()> {
+fn execute(orders: Vec<impl IntoLazyFrame>, currency: schema::Currency) -> Result<()> {
     let mut yahoo_scraper = Yahoo::new();
     let mut df = LazyFrame::default();
     for lf in orders {
@@ -53,7 +86,7 @@ fn execute(orders: Vec<impl IntoLazyFrame>) -> Result<()> {
         .with_quotes(&mut yahoo_scraper)?
         .with_average_price()?
         .with_uninvested_cash(cash.clone())
-        .normalize_currency(&mut yahoo_scraper, schema::Currency::GBP)?
+        .normalize_currency(&mut yahoo_scraper, currency)?
         .paper_profit()
         .with_dividends(dividends.clone())
         .with_profit()
